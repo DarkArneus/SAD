b8afabd88741f47c46a7ce2df939c920b6728946
Arnau Sola López <asola268@gmail.com>
2024-09-27 20:10:00 +0200

    Imports + arreglar tipo EditableBufferedReader

--- P-1/EditableBufferedReader.java
+++ P-1/EditableBufferedReader.java
@@ -1,26 +1,39 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 
 class EditableBufferedReader extends BufferedReader{
-  public EditableBufferedReader(Reader in) {
+  static final int ESC = 27;
+
+  EditableBufferedReader(InputStreamReader in) {
     super(in);
     //TODO Auto-generated constructor stub
   }
 
-  public static void setRaw(){
-
+  public static void setRaw() throws IOException{
+    String[] cmd = {"/bin/sh", "-c", "stty -echo raw </dev/tty"};
+    Runtime.getRuntime().exec(cmd);
   }
 
-  public static void unsetRaw(){
-
+  public static void unsetRaw() throws IOException{
+    String[] cmd = {"/bin/sh", "-c", "stty -echo cooked </dev/tty"};
+    Runtime.getRuntime().exec(cmd);
   }
   @Override
   public int read(){
-    return 0;
+    int cha = 0;
+    cha = super.read(); // leemos caracter
 
+    while(cha != ESC){
+      return cha;
+    }
+    return -1;
   }
+  
+  @SuppressWarnings("deprecation")
   @Override
-  public String readLine(){
+  public String readLine() {
     return null;
-  }
+}
 }
