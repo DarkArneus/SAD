8a941ae66035139e9b2e7e6d9801e53c2434f29c
Arnau Sola López <asola268@gmail.com>
2024-10-07 20:21:26 +0200

    TODO: manejar line

--- P-1/EditableBufferedReader.java
+++ P-1/EditableBufferedReader.java
@@ -10,10 +10,20 @@ class EditableBufferedReader extends BufferedReader {
 
   static final int LEFT = 68; // escape sequence de la flecha izq ^[[D D en ascii es 68
   static final int RIGHT = 67;
+  static final int HOME = 72; // ^[[H H es 72 en ascii
+  static final int END = 70;
+  static final int SUPR = 51; // ^[[3~ --> puede afectar a la escalabilidad ya que ^[[3J tiene funcionalidades como borrar toda la consola y habria que hacer mas distinciones y parsear mejor
+                              // si queremos en un futuro añadir mas funcionalidades como por ejemplo tiene VIM.
+  static final int INS = 50;   // ^[[2~
+  static final int BKSP = 127;  // 
 
-  static final int LEFT_RET = -1; // como ascii no tiene valores negativos vamos a usarlos para nuestros
-                                  // caracteres especiales
+  static final int LEFT_RET = -1; // como ascii no tiene valores negativos vamos a usarlos para nuestros caracteres especiales 
   static final int RIGHT_RET = -2;
+  static final int HOME_RET = -3;
+  static final int END_RET = -4;
+  static final int SUPR_RET = -5;
+  static final int INS_RET = -6;
+  static final int BKSP_RET = -7;
 
   EditableBufferedReader(InputStreamReader in) {
     super(in);
@@ -64,7 +74,7 @@ class EditableBufferedReader extends BufferedReader {
     int cha = 0;
     try {
       cha = super.read();// leemos caracter
-      if (cha == ESC) {
+      if (cha == ESC) { 
         cha = super.read();
         if (cha == CORCHETE) {
           cha = super.read();
@@ -73,6 +83,17 @@ class EditableBufferedReader extends BufferedReader {
               return LEFT_RET;
             case RIGHT:
               return RIGHT_RET;
+            case HOME:
+              return HOME_RET;
+            case END:
+              return END_RET;
+            case INS:
+              return INS_RET;
+            case SUPR:
+              return SUPR_RET;
+            case BKSP:
+              return BKSP_RET;
+
           }
         }
       }
@@ -92,8 +113,33 @@ class EditableBufferedReader extends BufferedReader {
 
     lectura = this.read();
     while (lectura != ENTER) {
-      System.out.print((char) lectura);
       lectura = this.read();
+      switch (lectura) {
+        case LEFT_RET:
+          System.out.print("\033[D"); // Usamos la escape sequence para movernos a la izquierda \033 es ESC
+        break;
+        case RIGHT_RET:
+          System.out.print("\033[C"); // Usamos la escape sequence para movernos a la derecha
+        break;
+        case HOME_RET:
+          System.out.print("\033[H");
+        break;
+        case END_RET:
+          System.out.print("\033[F");
+        break;
+        case INS_RET:
+          System.out.print("");
+        break;
+        case SUPR_RET:
+          System.out.print("\033[C");
+          System.out.print("\033[P");
+        break;
+        case BKSP_RET:
+          System.out.print("");
+        break;
+        default:
+          System.out.print((char) lectura); // Si no es ningun caracter especial printeamos la tecla directamente con un cast a char
+      }
     }
 
     EditableBufferedReader.unsetRaw(); // quitamos modo raw para volver al estado normal de la terminal
